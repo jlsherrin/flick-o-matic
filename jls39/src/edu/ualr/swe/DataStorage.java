3368844f995245e6169583562b283854058ca853
jlsherrin@gmail.com <jlsherrin@gmail.com@dc689ed6-e2d3-a3cb-88e9-6b8c2f9badd0>
2011-11-18 00:32:32 +0000

    Broke

---
+++ jls39/src/edu/ualr/swe/DataStorage.java
@@ -0,0 +1,61 @@
+package edu.ualr.swe;
+//import java.io.*;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+
+public class DataStorage extends Activity{
+	private String PREF_FILE_NAME = "prefFile";
+	private SharedPreferences preferences;
+	private SharedPreferences.Editor prefEditor;
+	
+	public DataStorage() {
+		preferences = getSharedPreferences(PREF_FILE_NAME, MODE_PRIVATE);
+		prefEditor = preferences.edit();
+		if (preferences.contains("timestamp")){
+			
+		}
+		else{
+			prefEditor.putString("oAuthToken", "");
+			prefEditor.putString("oAuthSecret","");
+			prefEditor.putBoolean("settings", true);
+			prefEditor.putLong("timestamp",System.currentTimeMillis()/1000);
+		}
+	}
+	
+	//for the user authentication section
+	public void saveAuth(String[] newAuth) {
+		prefEditor.putString("oAuthToken", newAuth[0]);
+		prefEditor.putString("oAuthSecret", newAuth[1]);
+		prefEditor.putLong("timestamp",System.currentTimeMillis()/1000);
+	}
+	public void delAuth() {
+		prefEditor.putString("oAuthToken", "");
+		prefEditor.putString("oAuthSecret","");
+	}
+
+	//for the upload section
+	public String[] getUploadData() {
+		String[] uploadData = new String[3];
+		uploadData[0] = preferences.getString("oAuthToken", "");
+		uploadData[1] = preferences.getString("oAuthSecret", "");
+		uploadData[2] = ""+preferences.getLong("timestamp", System.currentTimeMillis()/1000);
+		return uploadData;
+	}
+	public void updateTimestamp(){
+		prefEditor.putLong("timestamp",System.currentTimeMillis()/1000);
+	}
+	
+	//for display
+	public long getTimestamp(){
+		return preferences.getLong("timestamp", System.currentTimeMillis()/1000);
+	}
+	
+	//for settings section
+	public boolean getUpload(){
+		return preferences.getBoolean("settings", true);
+	}
+	public void setUpload(boolean nSet) {
+		prefEditor.putBoolean("settings",nSet);
+	}
+}
